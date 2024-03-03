58c52c4c3744797292828963c7ce66d23b430ba1
mzalog <95581915+mzalog@users.noreply.github.com>
2024-03-03 11:44:33 +0100

    Dodaj pliki projektów.

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_lab0
+{
+    public class FizzBuzz
+    {
+        private int upperLimit;
+
+        public FizzBuzz(int upperLimit)
+        {
+
+            this.upperLimit = upperLimit;
+
+        }
+
+        public void Display()
+        {
+            for (int i = 1; i <= upperLimit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0) { Console.WriteLine("Fizz"); }
+                else if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+                else { Console.WriteLine(i); }
+
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz_lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj gorny zakres:");
+            int upperLimit = Convert.ToInt32(Console.ReadLine());
+            FizzBuzz fizzBuzz = new FizzBuzz(upperLimit);
+            fizzBuzz.Display();
+        }
+    }
+}
